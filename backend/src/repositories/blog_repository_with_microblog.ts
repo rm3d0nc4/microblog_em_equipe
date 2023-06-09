58f43d14b9902c2ede3c90058bdb457e891711ff
Romero Antonio Ramos de Mendonça <romeromendonca22@gmail.com>
2023-06-09 17:27:23 -0300

    fixed blog repository implementation with microblog

--- backend/src/repositories/blog_repository_with_microblog.ts
+++ backend/src/repositories/blog_repository_with_microblog.ts
@@ -5,8 +5,9 @@ import BlogRepository from "../core/contracts/blog_repository";
 class BlogRepositoryWithMicroblog implements BlogRepository {
     microblog: Microblog = new Microblog();
     
-    async createPost(post: Post): Promise<void> {
-        return this.microblog.create(post);
+    async createPost(post: Post): Promise<Post> {
+        await this.microblog.create(post);
+        return post;
     }
     async deletePost(id: string): Promise<void> {
         this.microblog.delete(id);
