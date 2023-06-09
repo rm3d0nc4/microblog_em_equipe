e215dbbf015863f1e5022c68f23b032945cd069c
Romero Antonio Ramos de Mendonça <romeromendonca22@gmail.com>
2023-06-09 17:26:20 -0300

    changed blog repository implementation: implemented user repository contract; renamed userId paramenter in all methos

--- backend/src/repositories/blog_repository_with_database.ts
+++ backend/src/repositories/blog_repository_with_database.ts
@@ -7,11 +7,14 @@ import CommentableBLogRepository from "../core/contracts/commentable_blog_reposi
 import Comment from "../core/contracts/comment";
 import SComment from "../databases/sequelize/models/s_comment";
 import CommentImpl from '../core/entities/comment_impl';
+import UserBlogRepository from "../core/contracts/user_blog_repository";
+import User from "../core/contracts/user";
+import SUser from "../databases/sequelize/models/s_user";
 
-class BlogRepositoryWithDatabase implements BlogRepository, CommentableBLogRepository {
+class BlogRepositoryWithDatabase implements BlogRepository, CommentableBLogRepository, UserBlogRepository {
     
     async createPost(post: Post): Promise<Post> {
-        const newPost = await SPost.create({id: post.id, userId: post.userId, title: post.title, text: post.text, likes: post.likes})
+        const newPost = await SPost.create({id: post.id, SUserId: post.SUserId, title: post.title, text: post.text, likes: post.likes})
         return newPost.get() as PostImpl;
     }
     
@@ -19,7 +22,7 @@ class BlogRepositoryWithDatabase implements BlogRepository, CommentableBLogRepos
         const post = await this.retrievePost(id)
         await SPost.destroy({where: {id: post.id}});
     }
-
+    
     async retrievePost(id: string): Promise<Post> {
         const post = await SPost.findByPk(id);
         if (post) {
@@ -28,14 +31,14 @@ class BlogRepositoryWithDatabase implements BlogRepository, CommentableBLogRepos
             throw new AppError('Post Não Encontrado', 404)
         }
     }
-
+    
     async retrieveAllPosts(): Promise<Post[]> {
         const posts: Post[] = (await SPost.findAll()).map((post) => {
             return post.get() as PostImpl
         });
         return posts;
     }
-
+    
     async updatePost(post: Post): Promise<void> {
         const oldPost = await this.retrievePost(post.id)
         await SPost.update({title: post.title, text: post.text, likes: post.likes}, {where: {id: oldPost.id}});
@@ -43,7 +46,7 @@ class BlogRepositoryWithDatabase implements BlogRepository, CommentableBLogRepos
     
     async createComment(postId: string, comment: Comment): Promise<Comment> {
         this.retrievePost(postId);
-        const newComment = await SComment.create({id: comment.id, userId: comment.userId, text: comment.text, postId: postId})
+        const newComment = await SComment.create({id: comment.id, SUserId: comment.SUserId, text: comment.text, SPostId: postId})
         
         return newComment.get() as CommentImpl
     }
@@ -59,7 +62,7 @@ class BlogRepositoryWithDatabase implements BlogRepository, CommentableBLogRepos
             throw new AppError('Comentário não encontrado', 404)
         }
     }
-
+    
     async retrieveAllCommentsByPost(postId: string): Promise<Comment[]> {
         const comments = await SComment.findAll({
             where:{ postId: postId },
@@ -69,20 +72,37 @@ class BlogRepositoryWithDatabase implements BlogRepository, CommentableBLogRepos
         })
         return comments.map((comment) => comment.get() as CommentImpl);
     }
-
+    
     async retrieveAllComments(): Promise<Comment[]> {
         const comments = await SComment.findAll()
         return comments.map((comment) => comment.get() as CommentImpl);
     }
-
+    
     async updateComment(postId: string, comment: Comment): Promise<void> {
         const currentComment = await this.retrieveCommnent(postId, comment.id)
         await SComment.update({text: comment.text}, {where: {postId: postId, id: currentComment.id}})
     }
-
+    
     async deleteComment(postId: string, commentId: string): Promise<void> {
         const comment = await this.retrieveCommnent(postId, commentId)
         await SComment.destroy({where: {postId: postId, id: comment.id}})
+    }
+    
+    async createUser(user: User): Promise<void> {
+        
+        await SUser.create({id: user.id, name: user.name, email: user.email, password: user.password })
+    }
+    async retrieveUser(email: string): Promise<User> {
+        const user = await SUser.findOne({where: {email: email}})
+        if(user) {
+            return user.get() as User;
+        }
+        throw new AppError('Usuário não encontrado!', 404)
+    }
+    async retrieveAllUsers(): Promise<User[]> {
+        const users =  await SUser.findAll();
+
+        return users.map((user) => user.get() as User);
     }
 }
 
