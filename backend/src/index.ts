75d2a505a2e25e7dfe84cfca96673ef0b8b70c21
Romero Antonio Ramos de Mendonça <romeromendonca22@gmail.com>
2023-06-09 17:32:39 -0300

    added auth routes in application

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,24 @@
 import express, { Application} from "express";
 import errorMiddleware from "./core/middlewares/error_middleware";
+import cors from "cors";
 import { postRoutes } from "./routes/post_routes";
 import { commentRoutes } from "./routes/comment_routes";
-import cors from "cors";
+import { authRoutes } from "./routes/auth_routes";
 
 const app: Application = express();
+
 app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors())
 
+app.use(authRoutes);
 app.use(postRoutes);
 app.use(commentRoutes);
 
 app.use(errorMiddleware);
 
-const port: number = 3000;
+const port = 3000;
 app.listen(port, () => console.log(`Servidor inicializado em http://localhost:${port}`))
 
