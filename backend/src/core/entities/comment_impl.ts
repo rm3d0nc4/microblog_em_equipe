7a1f99b4793368aacfd28d4c19a59412af58c4fc
Romero Antonio Ramos de Mendonça <romeromendonca22@gmail.com>
2023-06-09 17:21:50 -0300

    changed entities

--- backend/src/core/entities/comment_impl.ts
+++ backend/src/core/entities/comment_impl.ts
@@ -5,13 +5,13 @@ import {v1 as  uuidv1} from 'uuid';
 export default class CommentImpl implements Comment{
     id: string;
     text: string;
-    userId: string;
+    SUserId: string;
     createdAt?: Date;
 
-    constructor(text: string, userId: string, createdAt?: Date) {
+    constructor(text: string, SUserId: string, createdAt?: Date) {
         this.id = uuidv1();
         this.text = text;
-        this.userId = userId;
+        this.SUserId = SUserId;
         this.createdAt = createdAt;
     }
 }
--- backend/src/core/entities/post_impl.ts
+++ backend/src/core/entities/post_impl.ts
@@ -2,7 +2,7 @@ import Post from "../contracts/post";
 import {v1 as  uuidv1} from 'uuid';
 export class PostImpl implements Post {
     id: string;
-    userId: string;
+    SUserId: string;
     title: string;
     text: string;
     likes: number;
@@ -10,9 +10,9 @@ export class PostImpl implements Post {
     updatedAt?: Date;
 
 
-    constructor( title: string, userId: string, text: string, createdAt?: Date, updatedAt?: Date) {
+    constructor( title: string, SUserId: string, text: string, createdAt?: Date, updatedAt?: Date) {
         this.id = uuidv1();
-        this.userId = userId;
+        this.SUserId = SUserId;
         this.title = title;
         this.text = text;
         this.likes = 0;      
